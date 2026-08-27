0fc9b647aa5e0f2d1068e95e310e8c64983ab8b8
Luis <luis.maldonado@tecsup.edu.pe>
2026-08-27 00:05:30 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana02/app/src/main/java/com/maldonado/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,9 @@
+package com.maldonado.lab02carritokotlin
+
+class Carrito {
+    fun main(){
+        println("=========================================")
+        println("  CARRITO DE COMPRAS - TIENDA TECSUP")
+        println("=========================================")
+    }
+}
